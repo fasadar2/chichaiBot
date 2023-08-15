0d85a034237af8f1fa00e88715349757c2f9eba2
fasadar2 <78956659+fasadar2@users.noreply.github.com>
2023-08-15 10:39:21 +0300

    some fix

--- launchBot.py
+++ launchBot.py
@@ -2,8 +2,9 @@ from telegram import Update
 import handler.HandlersBuilder as handlers
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters, MessageHandler
 from telegram import TelegramObject
+from settings import settings
 if __name__ == '__main__':
-    aplication = ApplicationBuilder().token('6333189100:AAEEsML_0LPCMzdKGkm-xGql3uxmYHBQ__E').build()
+    aplication = ApplicationBuilder().token(settings.token).build()
     print("Bot started")
     aplication.add_handlers(handlers.CreateHandler())
     aplication.run_polling()
