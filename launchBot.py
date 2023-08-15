6e34de617caf43fb99c33e349d953e3ac34c98ae
fasadar2 <78956659+fasadar2@users.noreply.github.com>
2023-08-15 10:45:44 +0300

    Update launchBot.py

--- launchBot.py
+++ launchBot.py
@@ -2,6 +2,7 @@ from telegram import Update
 import handler.HandlersBuilder as handlers
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters, MessageHandler
 from telegram import TelegramObject
+from settings import settings
 if __name__ == '__main__':
     aplication = ApplicationBuilder().token(settings.token).build()
     print("Bot started")
